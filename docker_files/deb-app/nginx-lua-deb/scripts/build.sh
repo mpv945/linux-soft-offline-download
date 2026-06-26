a5f4be508334024b22fdefca96bc3d84f98ce657
mpv945 <zhenxindefeng@126.com>
2026-06-27 00:56:08 +0800

    准备制作deb安装包

--- docker_files/deb-app/nginx-lua-deb/scripts/build.sh
+++ docker_files/deb-app/nginx-lua-deb/scripts/build.sh
@@ -55,6 +55,6 @@ dpkg-deb -c /offline/nginx-lua_*.deb | head -200
 
 echo "解压到目录查看"
 mkdir /tmp/deb
-dpkg-deb -x /offline/nginx-lua_*.deb/tmp/deb
+dpkg-deb -x /offline/nginx-lua_*.deb /tmp/deb
 
 ls -R /tmp/deb
